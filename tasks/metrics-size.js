13f171a98589f16dddb036f11a2a906e2b3f6f67
pavlo.savushchyk <pavlo.savushchyk@globallogic.com>
2015-02-19 14:04:11 +0200

    fix path to viz

--- tasks/metrics-size.js
+++ tasks/metrics-size.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 module.exports = function(grunt) {
     grunt.registerTask('metrics-size', 'Collect size of source files', function () {
 
-        var visFile = "build/optimize/client/visualize.js",
+        var visFile = "build/optimized/client/visualize.js",
             conf = grunt.config(),
             module = conf.pkg.name,
             dir = "build/optimized/",
