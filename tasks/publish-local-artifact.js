091328a85873a424cf0688d0f0bae78e494fbc2a
nesterone <igornester@gmail.com>
2015-03-26 19:49:03 +0200

    -prepared task for maven local build implementation

---
+++ tasks/publish-local-artifact.js
@@ -0,0 +1,12 @@
+var mvn;
+
+
+module.exports = function(grunt) {
+    grunt.registerTask('publish-local-artifact','Find prepared overlay and publish to local maven repository', function(){
+    		// check if overlay exists
+    		// run maven on it
+    		//  done or say what's wrong
+
+    		//*imporove overlay to take version from 'version'
+    });
+};
