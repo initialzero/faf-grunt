c1891dda8639dc8124ec6480365ec6addcff7c4f
pavlo.savushchyk <zigfredead@gmail.com>
2015-07-16 04:56:37 +0300

    update copy-themes-overlay task, it makes modules folders when copies module's css

--- tasks/copy-themes-overlay.js
+++ tasks/copy-themes-overlay.js
@@ -9,9 +9,22 @@ module.exports = function(grunt) {
             "bi-dashboard"
         ];
 
-    function copyModuleThemes(path) {
+    function copyModuleThemes(path, module) {
         grunt.file.recurse(path, function(abspath, rootdir, subdir, filename) {
-            grunt.file.copy(abspath, overlayThemesDir + "/" + (subdir ? subdir + "/" : "") + filename);
+
+            if (typeof subdir === "undefined") {
+                // copy files from <module>/themes/*.* to overlay/themes/
+                grunt.file.copy(abspath, overlayThemesDir + "/" + filename);
+            } else {
+                // copy files from <module>/themes/<theme> to overlay/themes/<theme>/<module>
+                if (module !== "jrs-ui" && module !== "jrs-ui-pro") {
+                    subdir = subdir.split("/");
+                    subdir.splice(1, 0, module);
+                    subdir = subdir.join("/");
+                }
+
+                grunt.file.copy(abspath, [overlayThemesDir, subdir, filename].join("/"));
+            }
         });
     }
 
@@ -21,16 +34,16 @@ module.exports = function(grunt) {
         modules.forEach(function(module) {
             var path = moduleDir + module + "/themes";
             if (grunt.file.isDir(path)) {
-                copyModuleThemes(path);
+                copyModuleThemes(moduleDir + module + "/themes", module);
             }
         });
 
         if (currentModule === "jrs-ui") {
-            copyModuleThemes("themes");
+            copyModuleThemes("themes", "jrs-ui");
         }
         if (currentModule === "jrs-ui-pro") {
-            copyModuleThemes(moduleDir + "jrs-ui/themes");
-            copyModuleThemes("themes");
+            copyModuleThemes(moduleDir + "jrs-ui/themes", "jrs-ui");
+            copyModuleThemes("themes", "jrs-ui-pro");
         }
 
     });
